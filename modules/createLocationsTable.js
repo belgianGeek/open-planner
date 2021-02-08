62c4ddf5ad0447ced00a5fa52e29eac4df1d12f7
belgianGee <39968047+belgianGeek@users.noreply.github.com>
2021-02-08 15:48:57 +0100

    Locations table auto-filling improvements

--- modules/createLocationsTable.js
+++ modules/createLocationsTable.js
@@ -1,18 +1,24 @@
 const path = require('path');
 
-const createLocationsTable = client => {
+const createLocationsTable = (client, locations) => {
   let table = 'locations';
   client.query(`CREATE TABLE IF NOT EXISTS ${table} (
     id SERIAL PRIMARY KEY,
     name TEXT)`, (err, res) => {
       if (err) {
-        console.error(`Une erreur est survenue lors de la création de la table ${table} : ${JSON.stringify(err, null, 2)}`);
+        console.error(`Une erreur est survenue lors de la création de la table ${table} : ${err}`);
       } else {
         console.log(`La table ${table} existe déjà...`);
         client.query(`SELECT * FROM ${table}`)
           .then(res => {
             if (res.rowCount === 0 || res.rowCount === undefined || res.rowCount === null) {
-              client.query(`INSERT INTO ${table} VALUES(${process.env.LOCATIONS.split(',')})`)
+              let locations2add = '';
+
+              for (let location of locations) {
+                locations2add += `('${location.trim()}')  `;
+              }
+
+              client.query(`INSERT INTO ${table}(name) VALUES${locations2add.split('  ').join(',').replace(/,$/, '')}`)
                 .then(res => {
                   if (res.rowCount > 0) {
                     console.log(`${res.rowCount} enregistrements ont été ajoutés à la table ${table} ;-)`);
