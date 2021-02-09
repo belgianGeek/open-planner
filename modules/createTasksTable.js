77ad29aa73eacc224f42bced3937237cbe2e0d3f
belgianGee <maximevanderwegen@disroot.org>
2021-02-09 14:46:33 +0100

    DB schema simplification

--- modules/createTasksTable.js
+++ modules/createTasksTable.js
@@ -1,22 +1,27 @@
 module.exports = function createTasksTable(client) {
-    let table = `tasks`;
-    client.query(`CREATE TABLE IF NOT EXISTS ${table} (
+  let table = `tasks`;
+  client.query(`CREATE TABLE IF NOT EXISTS ${table} (
       id SERIAL PRIMARY KEY,
       applicant_name TEXT,
       applicant_firstname TEXT,
       request_date TIMESTAMPTZ,
-      assignment_fk INT,
+      location_fk INT,
+      user_fk INT,
       comment TEXT,
       status TEXT,
-      CONSTRAINT assignment_fk
-        FOREIGN KEY(assignment_fk)
-	       REFERENCES assignments(id)
-	        ON DELETE CASCADE
+      CONSTRAINT location_fk
+        FOREIGN KEY(location_fk)
+	       REFERENCES locations(id)
+	        ON DELETE CASCADE,
+      CONSTRAINT user_fk
+        FOREIGN KEY(user_fk)
+    	   REFERENCES users(id)
+    	    ON DELETE CASCADE
   )`, (err, res) => {
-      if (err) {
-        console.error(`Une erreur est survenue lors de la création de la table ${table} : ${err}`);
-      } else {
-        console.log(`La table ${table} existe déjà...`);
-      }
-    });
+    if (err) {
+      console.error(`Une erreur est survenue lors de la création de la table ${table} : ${err}`);
+    } else {
+      console.log(`La table ${table} existe déjà...`);
+    }
+  });
 }
