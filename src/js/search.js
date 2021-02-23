3219d8e2d7c7f504a750d5157ea449ff8858039b
belgianGee <maximevanderwegen@disroot.org>
2021-02-23 10:06:56 +0100

    Remove the context menu for non-admin users

--- src/js/search.js
+++ src/js/search.js
@@ -170,151 +170,146 @@ const search = () => {
 
       $('.search__results__container').fadeIn();
 
-      $('.search__results__container__row').contextmenu(function(e) {
-        const setMenuPosition = height => {
-          $('.context')
-            .css({
-              left: `${e.pageX}px`,
-              top: `${height}px`
-            })
-            .toggleClass('hidden flex');
-        }
-
-        // get the current mouse position
-        getMousePosition();
-
-        // Store the selected row in a variable
-        parent = $(this).attr('class').split(' ')[1];
-
-        e.preventDefault();
-
-        if ($('.search__container__select').val() !== 'out_requests') {
-          setMenuPosition(mousePosition.y);
-        } else {
-          setMenuPosition(e.pageY);
-        }
-
-
-        // Hide the context menu on left-click to prevent displaying it indefinitely
-        $('.search__results, .search__results *').click(function(e) {
-          if (e.target === this) {
+      // Only show the customized context menu is the user is admin
+      if ($('.context').length) {
+        $('.search__results__container__row').contextmenu(function(e) {
+          const setMenuPosition = height => {
             $('.context')
-              .removeClass('flex')
-              .addClass('hidden');
+              .css({
+                left: `${e.pageX}px`,
+                top: `${height}px`
+              })
+              .toggleClass('hidden flex');
           }
-        });
-      });
 
-      $('.context__list__item').click(() => {
-        $('.context')
-          .removeClass('flex')
-          .addClass('hidden');
-      });
+          // get the current mouse position
+          getMousePosition();
 
-      $('.context__list__item--modify').click(function() {
-        // Format the date to be year, Month (0-indexed) and the day
-        let date = $(`.${parent} .search__results__container__row__item--timestamp`).text();
+          // Store the selected row in a variable
+          parent = $(this).attr('class').split(' ')[1];
 
-        $('.wrapper').addClass('blur');
+          e.preventDefault();
 
-        $('.inRequests')
-          .addClass('absolute flex')
-          .removeClass('hidden');
+          if ($('.search__container__select').val() !== 'out_requests') {
+            setMenuPosition(mousePosition.y);
+          } else {
+            setMenuPosition(e.pageY);
+          }
 
-        // Show all the fields that can be modified for the request to be updated and assigned
-        $('.inRequests.absolute .inRequests__form__btnContainer__hide, .inRequests__form__requestInfo__row1__assignedWorker, .inRequests.absolute .inRequests__form__requestInfo__row1__status')
-          .removeClass('hidden')
-          .addClass('flex');
 
-        $('.inRequests__form__applicantInfo__location, .inRequests__form__requestInfo__row1 label')
-          .addClass('hidden')
-          .removeClass('flex');
-
-        // Fill in all the fields with the selected record data
-        $('.inRequests.absolute .inRequests__id').text($(`.${parent} .search__results__container__row__item--id`).text());
-        $('.inRequests.absolute .inRequests__form__applicantInfo__location').val($(`.search__container__select`).val());
-        $('.inRequests.absolute .inRequests__form__applicantInfo__name').val($(`.${parent} .search__results__container__row__item--name`).text());
-        $('.inRequests.absolute .inRequests__form__applicantInfo__firstname').val($(`.${parent} .search__results__container__row__item--firstname`).text());
-        $('.inRequests.absolute .inRequests__form__requestInfo__row1__requestDate').val(date);
-        $('.inRequests.absolute .inRequests__form__requestInfo__comment').val($(`.${parent} .search__results__container__row__item--body`).text().replace('<br>', '\n'));
-        $('.inRequests.absolute .inRequests__form__requestInfo__row1__assignedWorker').val($(`.${parent} .search__results__container__row__item--awid`).text());
-
-        // Request status
-        if ($(`.${parent} .search__results__container__row__item--status`).hasClass('wip')) $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('wip');
-        else if ($(`.${parent} .search__results__container__row__item--status`).hasClass('waiting')) $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('waiting');
-        else $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('done');
-
-        // The form submit is handled in the inRequests function !
-        $('.inRequests.absolute .inRequests__form__btnContainer__submit').click(() => {
-          // Update the web interface with the changes
-          $(`.${parent} .search__results__container__row__item--name`).text($('.inRequests__form__applicantInfo__name').val().replace(/\'\'/g, "'"));
-          $(`.${parent} .search__results__container__row__item--firstname`).text($('.inRequests__form__applicantInfo__firstname').val().replace(/\'\'/g, "'"));
-          $(`.${parent} .search__results__container__row__item--date`).text(new Date($('.inRequests__form__requestInfo__row1__requestDate').val()).toLocaleDateString());
-          $(`.${parent} .search__results__container__row__item--location`).text($('.inRequests__form__applicantInfo__location option:selected').text().replace(/\'\'/g, "'"));
-          $(`.${parent} .search__results__container__row__item--body`).text($('.inRequests__form__requestInfo__comment').val().replace('\n', '<br>'));
-          $(`.${parent} .search__results__container__row__item--aw`).text($('.inRequests__form__requestInfo__row1__assignedWorker option:selected').text().replace(/\'\'/g, "'"));
-
-          $(`.${parent} .search__results__container__row__item--status`)
-            .removeClass('waiting wip done')
-            .addClass($('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val());
-
+          // Hide the context menu on left-click to prevent displaying it indefinitely
+          $('.search__results, .search__results *').click(function(e) {
+            if (e.target === this) {
+              $('.context')
+                .removeClass('flex')
+                .addClass('hidden');
+            }
+          });
         });
 
-        // Hide the form on btn click
-        $('.inRequests.absolute .inRequests__form__btnContainer__hide').click(function() {
-          $('.inRequests')
-            .removeClass('absolute flex')
+        $('.context__list__item').click(() => {
+          $('.context')
+            .removeClass('flex')
             .addClass('hidden');
-
-          $('.wrapper').removeClass('blur backgroundColor');
-
-          // Hide the button to hide the form
-          $(this).addClass('hidden');
         });
-      });
 
-      $('.context__list__item--del').click(function() {
-        let record2delete = {
-          key: $(`.${parent} .search__results__container__row__item--id`).text(),
-          table: $('.search__container__select').val()
-        };
+        $('.context__list__item--modify').click(function() {
+          // Format the date to be year, Month (0-indexed) and the day
+          let date = $(`.${parent} .search__results__container__row__item--timestamp`).text();
 
-        confirmation();
+          $('.wrapper').addClass('blur');
 
-        // Hide the record from the interface
-        $(`.${parent}`).toggleClass('hidden flex');
+          $('.inRequests')
+            .addClass('absolute flex')
+            .removeClass('hidden');
 
-        recordDelTimeOut = setTimeout(() => {
-          // Delete the record from the interface
-          $(`.${parent}`).remove();
-          confirmation();
-
-          socket.emit('delete data', record2delete);
-
-          // Reset the deletionKey
-          record2delete = {};
-
-        }, 5000);
-
-        $('.confirmation__body__cancel').click(() => {
-          clearTimeout(recordDelTimeOut);
-          $(`.${parent}`)
+          // Show all the fields that can be modified for the request to be updated and assigned
+          $('.inRequests.absolute .inRequests__form__btnContainer__hide, .inRequests__form__requestInfo__row1__assignedWorker, .inRequests.absolute .inRequests__form__requestInfo__row1__status')
             .removeClass('hidden')
             .addClass('flex');
-          recordDelTimeOut = undefined;
 
-          // Reset the deletionKey
-          record2delete = {};
+          $('.inRequests__form__applicantInfo__location, .inRequests__form__requestInfo__row1 label')
+            .addClass('hidden')
+            .removeClass('flex');
+
+          // Fill in all the fields with the selected record data
+          $('.inRequests.absolute .inRequests__id').text($(`.${parent} .search__results__container__row__item--id`).text());
+          $('.inRequests.absolute .inRequests__form__applicantInfo__location').val($(`.search__container__select`).val());
+          $('.inRequests.absolute .inRequests__form__applicantInfo__name').val($(`.${parent} .search__results__container__row__item--name`).text());
+          $('.inRequests.absolute .inRequests__form__applicantInfo__firstname').val($(`.${parent} .search__results__container__row__item--firstname`).text());
+          $('.inRequests.absolute .inRequests__form__requestInfo__row1__requestDate').val(date);
+          $('.inRequests.absolute .inRequests__form__requestInfo__comment').val($(`.${parent} .search__results__container__row__item--body`).text().replace('<br>', '\n'));
+          $('.inRequests.absolute .inRequests__form__requestInfo__row1__assignedWorker').val($(`.${parent} .search__results__container__row__item--awid`).text());
+
+          // Request status
+          if ($(`.${parent} .search__results__container__row__item--status`).hasClass('wip')) $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('wip');
+          else if ($(`.${parent} .search__results__container__row__item--status`).hasClass('waiting')) $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('waiting');
+          else $('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val('done');
+
+          // The form submit is handled in the inRequests function !
+          $('.inRequests.absolute .inRequests__form__btnContainer__submit').click(() => {
+            // Update the web interface with the changes
+            $(`.${parent} .search__results__container__row__item--name`).text($('.inRequests__form__applicantInfo__name').val().replace(/\'\'/g, "'"));
+            $(`.${parent} .search__results__container__row__item--firstname`).text($('.inRequests__form__applicantInfo__firstname').val().replace(/\'\'/g, "'"));
+            $(`.${parent} .search__results__container__row__item--date`).text(new Date($('.inRequests__form__requestInfo__row1__requestDate').val()).toLocaleDateString());
+            $(`.${parent} .search__results__container__row__item--location`).text($('.inRequests__form__applicantInfo__location option:selected').text().replace(/\'\'/g, "'"));
+            $(`.${parent} .search__results__container__row__item--body`).text($('.inRequests__form__requestInfo__comment').val().replace('\n', '<br>'));
+            $(`.${parent} .search__results__container__row__item--aw`).text($('.inRequests__form__requestInfo__row1__assignedWorker option:selected').text().replace(/\'\'/g, "'"));
+
+            $(`.${parent} .search__results__container__row__item--status`)
+              .removeClass('waiting wip done')
+              .addClass($('.inRequests.absolute .inRequests__form__requestInfo__row1__status').val());
+
+          });
+
+          // Hide the form on btn click
+          $('.inRequests.absolute .inRequests__form__btnContainer__hide').click(function() {
+            $('.inRequests')
+              .removeClass('absolute flex')
+              .addClass('hidden');
+
+            $('.wrapper').removeClass('blur backgroundColor');
+
+            // Hide the button to hide the form
+            $(this).addClass('hidden');
+          });
         });
-      });
 
-      // $('.context__list__item--inv').click(function() {
-      //   copyText($(this), parent, '.search__results__container__row__item--code', `Copier le n° d'inventaire`);
-      // });
-      //
-      // $('.context__list__item--pib').click(function() {
-      //   copyText($(this), parent, '.search__results__container__row__item--pib', `Copier le n° PIB`);
-      // });
+        $('.context__list__item--del').click(function() {
+          let record2delete = {
+            key: $(`.${parent} .search__results__container__row__item--id`).text(),
+            table: $('.search__container__select').val()
+          };
+
+          confirmation();
+
+          // Hide the record from the interface
+          $(`.${parent}`).toggleClass('hidden flex');
+
+          recordDelTimeOut = setTimeout(() => {
+            // Delete the record from the interface
+            $(`.${parent}`).remove();
+            confirmation();
+
+            socket.emit('delete data', record2delete);
+
+            // Reset the deletionKey
+            record2delete = {};
+
+          }, 5000);
+
+          $('.confirmation__body__cancel').click(() => {
+            clearTimeout(recordDelTimeOut);
+            $(`.${parent}`)
+              .removeClass('hidden')
+              .addClass('flex');
+            recordDelTimeOut = undefined;
+
+            // Reset the deletionKey
+            record2delete = {};
+          });
+        });
+      }
     }
   });
 }
