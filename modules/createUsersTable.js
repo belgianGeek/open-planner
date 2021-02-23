f5fde83846187cd032aacba1e1ad30471acbc264
belgianGee <maximevanderwegen@disroot.org>
2021-02-23 15:29:48 +0100

    Code cleanup :recycle:

--- modules/createUsersTable.js
+++ modules/createUsersTable.js
@@ -16,30 +16,12 @@ const createUsersTable = client => {
        REFERENCES locations(location_id)
         ON DELETE CASCADE
   )`, (err, res) => {
-      if (err) {
-        console.error(`Une erreur est survenue lors de la création de la table ${table} : ${err}`);
-      } else {
-        console.log(`La table ${table} existe déjà...`);
-        client.query(`SELECT * FROM ${table}`)
-          .then(res => {
-            if (res.rowCount === 0 || res.rowCount === undefined || res.rowCount === null) {
-              client.query(`COPY ${table}(name, firstname, email, location, gender, password, type) FROM '/${table}.csv' DELIMITER ',' CSV HEADER`)
-                .then(res => {
-                  if (res.rowCount > 0) {
-                    console.log(`${res.rowCount} enregistrements ont été ajoutés à la table ${table} ;-)`);
-                  } else {
-                    console.log(`${res.rowCount} enregistrement a été ajouté à la table ${table} ;-)`);
-                  }
-                })
-                .catch(err => {
-                  console.error(`Une erreur est survenue lors du remplissage de la table ${table} : ${err}`);
-                });
-            } else {
-              console.log(`La table ${table} est déjà remplie ;-)`);
-            }
-          });
-      }
-    });
+    if (err) {
+      console.error(`Une erreur est survenue lors de la création de la table ${table} : ${err}`);
+    } else {
+      console.log(`La table ${table} existe déjà...`);
+    }
+  });
 }
 
 module.exports = createUsersTable;
