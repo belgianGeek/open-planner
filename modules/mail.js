6b87927a68446ea5f620bef730999e808a0cc87a
belgianGee <maximevanderwegen@disroot.org>
2021-03-22 10:11:20 +0100

    Mail sending improvements, send mails on task modification :sparkles:

--- modules/mail.js
+++ modules/mail.js
@@ -1,76 +1,103 @@
-const env = require('dotenv').config();
-const nodemailer = require('nodemailer');
-const ejs = require('ejs');
-const ip = require('ip');
-const path = require('path');
+const mail = (app, io) => {
+  const nodemailer = require('nodemailer');
+  const DBquery = require('./DBquery');
+  const ejs = require('ejs');
+  const ip = require('ip');
+  const notify = require('./notify');
+  const path = require('path');
 
-async function mail(receiver, applicant, client, sendcc) {
-  let tempReceiverName;
+  async function mail(receiver, applicant, mailObject) {
+    let tempReceiverName;
 
-  if (receiver.name !== undefined) {
-    let tempReceiverName = receiver.name.split(',');
-  }
-
-  let htmlMsg = msg = '';
-
-  const res = await client.query(`SELECT * FROM settings`);
-  if (res.rowCount !== 0 && res.rowCount !== null) {
-
-    let sender = smtpUsername = res.rows[0].smtp_user;
-    let smtpPass = res.rows[0].smtp_passwd;
-
-    msg = await ejs.renderFile(path.join(__dirname, '../views', 'mail.ejs'), {
-      applicant: `${applicant.firstname} ${applicant.name.toUpperCase()}`,
-      gender: receiver.gender,
-      host: ip.address(),
-      location: applicant.location,
-      request: receiver.request.replace(/\'\'/g, "'"),
-      sender: res.rows[0].sender
-    });
-
-    const sendMail = () => {
-      // Create and send the mail
-      let mail = nodemailer.createTransport({
-        host: res.rows[0].smtp_host,
-        port: 587,
-        secure: false,
-        auth: {
-          user: smtpUsername,
-          pass: smtpPass
-        }
-      });
-
-      mail.verify(async (err, success) => {
-        if (!err) {
-          let options = {
-            from: `${res.rows[0].sender} <${sender}>`,
-            to: receiver.mail,
-            subject: "Une nouvelle demande a été introduite dans le tableau d'intervention",
-            html: msg
-          };
-
-          // Send a copy of the request to the applicant
-          if (sendcc) {
-            const cc = await client.query(`SELECT email FROM users WHERE name ILIKE '${applicant.name}' AND firstname ILIKE '${applicant.firstname}'`);
-            options.cc = cc.rows[0].email;
-          }
-
-          mail.sendMail(options).catch(err => console.trace(err));
-
-          if (!sendcc) {
-            console.log(`Mail envoyé à l'adresse ${receiver.mail} le ${new Date()}`);
-          } else {
-            console.log(`Mail envoyé aux adresses ${receiver.mail} et ${options.cc} le ${new Date()}`);
-          }
-        } else {
-          console.trace('Il semble que les paramètres SMTP spécifiés soient incorrects : ' + err);
-        }
-      });
+    if (receiver.name !== undefined) {
+      let tempReceiverName = receiver.name.split(',');
     }
-    sendMail();
-  } else {
-    console.trace(res.rowCount, res.rows);
+
+    let htmlMsg = msg = '';
+
+    const res = await app.client.query(`SELECT * FROM settings`);
+    if (res.rowCount !== 0 && res.rowCount !== null) {
+
+      let sender = smtpUsername = res.rows[0].smtp_user;
+      let smtpPass = res.rows[0].smtp_passwd;
+
+      msg = await ejs.renderFile(path.join(__dirname, '../views', 'mail.ejs'), {
+        applicant: `${applicant.firstname} ${applicant.name.toUpperCase()}`,
+        date: mailObject.creationDate,
+        gender: receiver.gender,
+        host: ip.address(),
+        id: mailObject.id,
+        location: applicant.location,
+        request: receiver.request.replace(/\'\'/g, "'"),
+        sender: res.rows[0].sender,
+        status: mailObject.status
+      });
+
+      const sendMail = () => {
+        // Create and send the mail
+        let mail = nodemailer.createTransport({
+          host: res.rows[0].smtp_host,
+          port: 587,
+          secure: false,
+          auth: {
+            user: smtpUsername,
+            pass: smtpPass
+          }
+        });
+
+        mail.verify(async (err, success) => {
+          if (!err) {
+            let options = {
+              from: `${res.rows[0].sender} <${sender}>`,
+              to: receiver.mail,
+              subject: mailObject.title,
+              html: msg
+            };
+
+            // Send a copy of the request to the applicant
+            if (mailObject.sendcc) {
+              const cc = await app.client.query(`SELECT email FROM users WHERE name ILIKE '${applicant.name}' AND firstname ILIKE '${applicant.firstname}'`);
+              options.cc = cc.rows[0].email;
+            }
+
+            mail.sendMail(options).catch(err => console.trace(err));
+
+            if (!mailObject.sendcc) {
+              console.log(`Mail envoyé à l'adresse ${receiver.mail} le ${new Date()}`);
+            } else {
+              console.log(`Mail envoyé aux adresses ${receiver.mail} et ${options.cc} le ${new Date()}`);
+            }
+          } else {
+            console.trace('Il semble que les paramètres SMTP spécifiés soient incorrects : ' + err);
+          }
+        });
+      }
+      sendMail();
+    } else {
+      console.trace(res.rowCount, res.rows);
+    }
   }
+
+  io.on('send mail', data => {
+    data.mail.sendcc = data.sendcc;
+
+    let receiver = {
+      mail: '',
+      request: data.request
+    };
+
+    let applicant = data.applicant;
+
+    DBquery(app, io, 'SELECT', 'users', {
+        text: `SELECT * FROM users INNER JOIN locations ON users.location = locations.location_id`
+      })
+      .then(res => {
+        receiver.mail = res.rows[0].location_mail;
+        // Do not set the receiver gender
+        mail(receiver, applicant, data.mail);
+        notify(io, 'mail');
+      });
+  });
 }
 
 module.exports = mail;
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,35 +1,4 @@
 const settings = () => {
-  const toggleSwitch = (input, slider, check = true) => {
-    if (check) {
-      $(input).prop('checked', true);
-      $(slider)
-        .removeClass('unchecked')
-        .addClass('checked');
-    } else {
-      $(input).prop('checked', false);
-      $(slider)
-        .removeClass('checked')
-        .addClass('unchecked');
-    }
-  }
-
-  // Get settings from the server-side
-  socket.on('settings', settings => {
-    // Update the global object with the retrieved settings
-    globalSettings = settings;
-    if (settings.sendcc) {
-      toggleSwitch('.toggleMailCc__Input', '.toggleMailCc__Slider', true);
-    } else {
-      toggleSwitch('.toggleMailCc__Input', '.toggleMailCc__Slider', false);
-    }
-
-    if (settings.sendmail) {
-      toggleSwitch('.toggleMail__Input', '.toggleMail__Slider', true);
-    } else {
-      toggleSwitch('.toggleMail__Input', '.toggleMail__Slider', false);
-    }
-  });
-
   // Show settings on btn click
   $('.settingsLink').click(() => {
     $('.settings__container').toggleClass('hidden flex');
@@ -50,7 +19,9 @@ const settings = () => {
         updatedSettings.sendcc = globalSettings.sendcc = $('.toggleMailCc__Input').prop('checked');
       }
 
-      socket.emit('settings', updatedSettings);
+      if (updatedSettings !== {}) {
+        socket.emit('settings', updatedSettings);
+      }
     }
   });
 }
