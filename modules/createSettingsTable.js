9f8397a117ed5956c2d0f04a916ea8df107b0cc3
belgianGeek <39968047+belgianGeek@users.noreply.github.com>
2021-04-07 15:03:55 +0200

    Add attachments sending permission

--- modules/createSettingsTable.js
+++ modules/createSettingsTable.js
@@ -9,11 +9,12 @@ const createSettingsTable = (client, data) => {
       smtp_host TEXT,
       smtp_passwd TEXT,
       wallpaper TEXT,
+      sendattachments BOOLEAN,
       sendcc BOOLEAN,
       sendmail BOOLEAN
   )`, (err, res) => {
       if (err) {
-        msg = `La création de la table 'settings' a échoué : ${JSON.stringify(err, null, 2)}`;
+        msg = `La création de la table 'settings' a échoué : ${err}`;
         reject(msg)
         return console.error(msg);
       } else {
@@ -25,8 +26,8 @@ const createSettingsTable = (client, data) => {
               console.log('Remplissage initial de la table \'settings\'...');
               let mailContent;
               client.query({
-                  text: `INSERT INTO settings(instance_name, sender, mail_address, smtp_user, smtp_host, smtp_passwd, wallpaper, sendcc, sendmail) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
-                  values: [data.instance, data.sender, data.smtp_user, data.smtp_user, data.smtp_host, data.smtp_passwd, '../src/scss/wallpaper.jpg', true, true]
+                  text: `INSERT INTO settings(instance_name, sender, mail_address, smtp_user, smtp_host, smtp_passwd, wallpaper, sendattachments, sendcc, sendmail) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+                  values: [data.instance, data.sender, data.smtp_user, data.smtp_user, data.smtp_host, data.smtp_passwd, '../src/scss/wallpaper.jpg', true, true, true]
                 })
                 .then(res => {
                   msg = 'Remplissage de la table \'settings\' effectué avec succès !';
--- routes/home.js
+++ routes/home.js
@@ -31,6 +31,7 @@ module.exports = function(app, io) {
       isSearchPage: false,
       locations: locations.rows,
       instanceName: app.open_planner_instance_name,
+      sendAttachments: userSettings.sendattachments,
       user: req.user
     });
 
@@ -44,7 +45,7 @@ module.exports = function(app, io) {
 
       io.on('append data', async data => {
         if (data.table === 'tasks') {
-          if (!data.sendAttachment) {
+          if (!data.sendattachment) {
             DBquery(app, io, 'INSERT INTO', data.table, {
               text: `INSERT INTO ${data.table}(applicant_name, applicant_firstname, comment, request_date, location_fk, status, attachment) VALUES($1, $2, $3, $4, $5, $6, $7)`,
               values: data.values
@@ -139,12 +140,20 @@ module.exports = function(app, io) {
 
       io.on('settings', settings => {
         query = ['UPDATE settings SET'];
+        if (settings.instance_name !== undefined) {
+          query.push(`instance_name = ${settings.instance_name},`);
+        }
+
         if (settings.sendmail !== undefined) {
           query.push(`sendmail = ${settings.sendmail},`);
         }
 
         if (settings.sendcc !== undefined) {
           query.push(`sendcc = ${settings.sendcc},`);
+        }
+
+        if (settings.sendattachments !== undefined) {
+          query.push(`sendattachments = ${settings.sendattachments},`);
         }
 
         if (settings.sender !== undefined) {
