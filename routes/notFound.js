3bfbbd3bade8b49e2f3853b43eec6b0f51681624
belgianGeek <maximevanderwegen@disroot.org>
2021-04-10 14:55:42 +0200

    Fix 404 error page :bug:

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,6 +1,6 @@
 module.exports = function(app, io) {
   const notFoundmessages = [
-    'Hey, il semble que vous soyez égaré ! Par contre, tu as trouvé Scrat, félicitations !',
+    'Hey, il semble que tu te sois égaré ! Par contre, tu as trouvé Scrat, félicitations !',
     'Coucou ! Tu sembles t\'être trompé.e de chemin !',
     'Désolé, il semble que cette page n\'existe pas ou plus...',
     'Ceci n\'est pas une page d\'erreur (Nan, j\'déconne, c\'est une vraie)',
@@ -10,7 +10,10 @@ module.exports = function(app, io) {
   app.use((req, res, next) => {
     res.status(404).render('404.ejs', {
       msg: notFoundmessages[Math.floor(Math.random() * notFoundmessages.length)],
-      instanceName: app.open_planner_instance_name
+      instanceName: app.open_planner_instance_name,
+      instance_description: app.open_planner_instance_description,
+      page_type: 'Page non trouvée',
+      route: req.path
     });
   });
 };
