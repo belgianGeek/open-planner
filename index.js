73df7fcbecd3c776274d4080a6ce93f6719b0f20
belgianGeek <maximevanderwegen@disroot.org>
2021-06-12 16:37:58 +0200

    Fix bug introduced in eda31d8a28de02cf5fe55ba99b6e838c3b89b8b0 :bug:

--- index.js
+++ index.js
@@ -28,23 +28,7 @@ let config = {
   }
 };
 
-const checkForPassword = () => {
-  if (fs.existsSync('.passwd')) {
-    config.password = fs.readFileSync('.passwd', {
-      encoding: 'utf-8'
-    });
-
-    config.password = config.password.replace(/[\n\r\s]/g, '');
-
-    config.connectionString = `postgresql://${config.user}:${config.password}@${config.host}:5432/${config.database}`;
-  } else {
-    config.connectionString = `postgresql://${config.user}@${config.host}:5432/${config.database}`;
-  }
-}
-
-checkForPassword();
-
-const initClient = new Pool(config);
+app.pool = new Pool(config);
 
 // Define a variable to store the settings retrieved from the DB
 let settings = {};
@@ -71,67 +55,6 @@ const createSessionTable = require('./modules/createSessionTable');
 const createTasksTable = require('./modules/createTasksTable');
 const createUsersTable = require('./modules/createUsersTable');
 
-const createDB = (config, DBname = 'planner') => {
-  const createTables = () => {
-    console.log(`Base de données ${DBname} créée avec succès, création des tables en cours...`);
-    app.pool = new Pool(config);
-
-    // Tables have to be created in this exact order to avoid errors when assigning foreign key constraints
-    app.pool.connect()
-      .then(() => {
-        createLocationsTable(app.pool)
-          .then(res => {
-            console.log(res);
-
-            createUsersTable(app.pool)
-              .then(res => {
-                console.log(res);
-
-                getUsers(app, passport);
-
-                createTasksTable(app.pool)
-                  .then(res => {
-                    console.log(res);
-                    console.log(`Tu peux te connecter à Open Planner ici : http://${ip.address()}:8000.`);
-                  });
-              })
-              .catch(err => console.log(err));
-          })
-          .catch(err => console.log(err));
-      })
-      .catch(err => {
-        console.trace(`Pool connection error : ${err}`);
-      });
-  }
-
-  const reconnect = () => {
-    // Disconnect from the 'postgres' DB and connect to the newly created 'node-planner' DB
-    config.database = 'planner';
-    checkForPassword();
-
-    initClient
-      .end()
-      .catch(err => console.error('Une erreur est survenue lors de la tentative de reconnexion à la base de données', err));
-  }
-
-  console.log(`Création de la base de données ${DBname}...`);
-  initClient.query(`CREATE DATABASE ${DBname} WITH ENCODING = 'UTF-8'`)
-    .then(res => {
-      reconnect();
-
-      createTables();
-    })
-    .catch(err => {
-      if (!err.message.match('exist')) {
-        console.error(`Erreur lors de la création de la base de données ${DBname} : ${err}`);
-      } else {
-        console.log(`La base de données ${DBname} existe déjà !`);
-        reconnect();
-        createTables();
-      }
-    });
-}
-
 existPath('./backups/');
 existPath('./exports/');
 existPath('./templates/');
@@ -146,52 +69,33 @@ setInterval(() => {
 // Define a variable to store a file to download when exporting DB
 app.file2download = {};
 
-initClient.connect()
+// Tables have to be created in this exact order to avoid errors when assigning foreign key constraints
+app.pool.connect()
   .then(() => {
-    if (!ip.address().match(/169.254/) || !ip.address().match(/127.0/)) {
-      // Check if the 'session' table exist before doing anything else
-      createSessionTable(initClient);
+    // Check if the 'session' table exist before doing anything else
+    createSessionTable(app.pool);
+    createLocationsTable(app.pool)
+      .then(res => {
+        console.log(res);
 
-      console.log(`Création des tables en cours...`);
-      app.pool = new Pool(config);
-
-      // Tables have to be created in this exact order to avoid errors when assigning foreign key constraints
-      app.pool.connect()
-        .then(() => {
-          createLocationsTable(app.pool)
+        createUsersTable(app.pool)
           .then(res => {
             console.log(res);
 
-            createUsersTable(app.pool)
-            .then(res => {
-              console.log(res);
+            getUsers(app, passport);
 
-              getUsers(app, passport);
-
-              createTasksTable(app.pool)
+            createTasksTable(app.pool)
               .then(res => {
                 console.log(res);
                 console.log(`Tu peux te connecter à Open Planner ici : http://${ip.address()}:8000.`);
               });
-            })
-            .catch(err => console.log(err));
           })
           .catch(err => console.log(err));
-        })
-        .catch(err => {
-          console.trace(`Pool connection error : ${err}`);
-        });
-    } else {
-      console.log(`Désolé, il semble que tu n'aies pas accès à Internet... Rétablis ta connexion et réessaie :-)`);
-    }
+      })
+      .catch(err => console.log(err));
   })
   .catch(err => {
-    console.log(`Initial Pool connection error : ${err}`);
-    if (err.code === 'ECONNREFUSED') {
-      console.log('Désolé, la connexion à la base de données n\'a pas pu être établie...\n' +
-        'Vérifie que le service PostgreSQL est bien démarré et relance Node Planner.');
-    }
-    return;
+    console.trace(`Pool connection error : ${err}`);
   });
 
 app.set('view engine', 'ejs');
