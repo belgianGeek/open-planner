77f671ff0f98278239baf03d032803c1e4e3d7ba
belgianGee <maximevanderwegen@disroot.org>
2021-06-24 18:15:47 +0200

    Update prepared queries, frontend bug fixes and improvements :bug:

--- modules/updateTask.js
+++ modules/updateTask.js
@@ -1,15 +1,15 @@
-module.exports = async function updateTask(query) {
+module.exports = async function updateTask(query, record) {
   query.name = 'update-task';
 
   if (!record.sendattachment) {
     query = {
-      text: `UPDATE ${record.table} SET applicant_name = $1, applicant_firstname = $2, comment = $3, status = $4, user_fk = $5 WHERE task_id = ${record.id}`,
-      values: [record.values[0], record.values[1], record.values[2], record.values[3], record.values[4]]
+      text: `UPDATE ${record.table} SET applicant_name = $1, applicant_firstname = $2, comment = $3, status = $4, user_fk = $5, attachment = $6 WHERE task_id = ${record.id}`,
+      values: [record.values[0], record.values[1], record.values[2], record.values[3], record.values[4], record.values[5]]
     }
   } else {
     query = {
-      text:`UPDATE ${record.table} SET applicant_name = $0, applicant_firstname = $1, comment = $2, status = $3, user_fk = $4, attachment = $5, attachment_src = $6 WHERE task_id = ${record.id}`,
-      values: [record.values[0], record.values[1], record.values[2], record.values[3], record.values[4], record.values[5]]
+      text:`UPDATE ${record.table} SET applicant_name = $1, applicant_firstname = $2, comment = $3, status = $4, user_fk = $5, attachment = $6, attachment_src = $7 WHERE task_id = ${record.id}`,
+      values: [record.values[0], record.values[1], record.values[2], record.values[3], record.values[4], record.values[5], record.values[6]]
     }
   }
 
