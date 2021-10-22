a8f7bbdb37aebcd010bb506918c4d7edee734bec
belgianGee <maximevanderwegen@disroot.org>
2021-10-22 18:48:05 +0200

    Send a notification mail on task deletion :sparkles:

--- src/js/settings.js
+++ src/js/settings.js
@@ -55,6 +55,10 @@ const settings = () => {
         updatedSettings.displaymyrequestsmenu = globalSettings.displaymyrequestsmenu = $('.toggleMyRequests__Input').prop('checked');
       }
 
+      if (globalSettings.sendrequestdeletionmail !== $('.toggleRequestDeletionMail__Input').prop('checked')) {
+        updatedSettings.sendrequestdeletionmail = globalSettings.sendrequestdeletionmail = $('.toggleRequestDeletionMail__Input').prop('checked');
+      }
+
       if (Object.keys(updatedSettings).length > 0) {
         socket.emit('settings', updatedSettings);
       }
